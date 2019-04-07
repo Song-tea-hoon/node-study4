ee25d940ac9e9f73c0202c0dae11eb6be9f3ac1c
Your Name <you@example.com>
2019-04-07 15:00:26 +0900

    socket.io test : of이벤트 와 socket이벤트 생성

--- node_proj04_socketIO_test01/app_ex04_namespace.js
+++ node_proj04_socketIO_test01/app_ex04_namespace.js
@@ -15,4 +15,15 @@ var io = socketio.listen(server);
 // of(경로).on(이벤트 ,function{});
 var chat = io.of('/chat').on('connection', function(socket){
    console.log('chat connection event ...');
+    
+    //chat 변수와 socket변수로 데이터 확인 테스트
+    socket.emit('socket message', {
+        that: 'only',
+        '/chat': 'will get'
+    });
+    
+    chat.emit('chat message', {
+        that: 'in',
+        '/chat': 'will get'
+    });
 });
