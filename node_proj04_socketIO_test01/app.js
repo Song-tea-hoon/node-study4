bc25a6b54d6567601316273336f13c09f11759cf
Your Name <you@example.com>
2019-04-07 12:48:22 +0900

    socket.io test : socket.io재설치 및 socket.io기본 설

---
+++ node_proj04_socketIO_test01/app.js
@@ -0,0 +1,10 @@
+// socket호출과 동시에 listen 3000번 실행
+const io = require('socket.io')(3000);
+
+io.sockets.on('connection', function(socket){
+    console.log('sockets connection');
+    socket.emit('news', {hello:'world'});
+    socket.on('my other event', function(data){
+        console.log(data);
+    })
+})
