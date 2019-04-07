b0f307f315d1765a8efbd22ab2aef0691e5ad787
Your Name <you@example.com>
2019-04-07 10:49:04 +0900

    counter : 카운트 추가

--- node_proj02_counter/app.js
+++ node_proj02_counter/app.js
@@ -7,8 +7,21 @@ const router = express.Router();
 app.set('port', 3000)
 app.use(cors());
 
+let cnt = 0;
+router.route('/count').get( (req, res)=>{
+    console.log('/count 요청 됨')
+    cnt++;
+    let date = new Date();
+    let resData = {
+        "dateStr": date.getHours() + " : " + date.getMinutes(),
+        "count": cnt
+    }
+    
+    res.end(JSON.stringify(resData));
+})
+
 app.use('/', router);
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
-    console.log('localhost:', app.get('port'));
+    console.log('http://localhost:' + app.get('port'));
 })
