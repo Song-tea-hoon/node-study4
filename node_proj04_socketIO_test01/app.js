bf702b13e9804a5cda76e5cacd5bb6929f95a929
Your Name <you@example.com>
2019-04-07 13:00:43 +0900

    socket.io test : client connect이벤트 추가

--- node_proj04_socketIO_test01/app.js
+++ node_proj04_socketIO_test01/app.js
@@ -1,6 +1,7 @@
 // socket호출과 동시에 listen 3000번 실행
 const io = require('socket.io')(3000);
 
+// 서버는 connection / client는 connect
 io.sockets.on('connection', function(socket){
     console.log('sockets connection');
     socket.emit('news', {hello:'world'});
