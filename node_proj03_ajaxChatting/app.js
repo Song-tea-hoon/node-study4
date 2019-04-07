10cc4598070d826f9d79b0dd9dbf9e4b1b77cfac
Your Name <you@example.com>
2019-04-07 11:19:13 +0900

    no socket 채팅 : recieve / send구현

--- node_proj03_ajaxChatting/app.js
+++ node_proj03_ajaxChatting/app.js
@@ -7,6 +7,29 @@ const router = express.Router();
 app.set('port', 3000);
 app.use(cors());
 
+let messages = [];
+
+router.route("/recieve", (req, res) => {
+    if(req.query,size >= messages.length ) {
+        res.end();
+    } else {
+        let result = {
+            total: messages.length,
+            messages: messages.slice(req.query.size)
+        }
+        
+        res.end(JSON.stringify(result));
+    }
+});
+
+router.route("/send", (req, res) => {
+    messages.push({
+        sender: req.query.sender,
+        maessage: req.query.message
+    });
+    res.end();
+});
+
 app.use('/', router);
 const server = http.createServer(app);
 server.listen(app.get('port'), ()=>{
