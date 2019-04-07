30466d64b70441af5a8f832b8dcb436facad525f
Your Name <you@example.com>
2019-04-07 09:58:10 +0900

    계산기 : js생성 및 router설정

--- node_proj01_calc/app.js
+++ node_proj01_calc/app.js
@@ -1,9 +1,33 @@
 const http = require('http');
 const express = require('express');
 const app = express();
-// app.set('port', 8888);
+const router = express.Router();
+app.set('port', 8888);
 
+router.route("/plus/:a/:b").get((req, res)=>{
+    let a = req.params.a; // 문자열 타입
+    let b = req.params.b; 
+    console.log(a, b)
+    let result = Number(a) + Number(b); // number type
+    res.end(String(result));
+})
+
+router.route("/minus/:a/:b").get((req, res)=>{
+    let a = req.params.a; // 문자열 타입
+    let b = req.params.b; 
+    res.end(String(parseInt(a) + parseInt(b)));
+})
+
+router.route("/mult/:a/:b").get((req, res)=>{
+    res.end(String(parseInt(req.params.a) * parseInt(req.params.b)));
+})
+
+router.route("/div/:a/:b").get((req, res)=>{
+    res.end(String(parseInt(req.params.a) / parseInt(req.params.b)));
+})
+
+app.use('/', router);
 const server = http.createServer(app);
-app.listen(8888, function() {
-    console.log('localhost:8888');
+app.listen(app.get('port'), () => {
+    console.log('localhost:', app.get('port'));
 })
