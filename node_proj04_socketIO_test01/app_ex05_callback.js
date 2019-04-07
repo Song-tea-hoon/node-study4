55098efbeb723fbec91b10fcfbcab0b73a39a713
Your Name <you@example.com>
2019-04-07 15:39:50 +0900

    socket.io test : callback

--- node_proj04_socketIO_test01/app_ex05_callback.js
+++ node_proj04_socketIO_test01/app_ex05_callback.js
@@ -6,3 +6,16 @@ io.sockets.on('connection', function(socket){
         callback('SUCCESS!');
     });
 });
+
+/*
+<script>
+    var socket = io.connect('http://localhost:3000');
+    socket.on('connect', function(){
+        console.log('서버에 소켓 연결');
+        // 이벤트 명, 데이터, callback
+        socket.emit('message1', 'HONG', function(msg){
+            console.log('서버에서 보낸 메세지>>> ', msg);
+        })
+    });
+</script>
+*/
